7f22f1428edb75620861574f2fdc285c21d1d6d8
Stav Homsky <stav.homsky@campus.technion.ac.il>
2026-03-19 12:21:46 +0200

    Add module function 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sysinfo
+import logcheck
 
 print("- [1] System Info")
 print("- [2] Log Checker")
@@ -9,6 +10,9 @@ while(user_select!= '0'):
     if user_select == '1':
         print("Calling show_sysinfo()... ")
         sysinfo.show_sysinfo()
+    elif user_select == '2':
+        print("Caliing check_log()... ")
+        logcheck.check_log()
     print("- [1] System Info")
     print("- [2] Log Checker")
     print("- [3] Task List")
