641a0b20ecb76ab564a87f0ee0c3e801819972c0
Stav Homsky <stav.homsky@campus.technion.ac.il>
2026-03-19 12:18:46 +0200

    Add module function 1

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import sysinfo
+
 print("- [1] System Info")
 print("- [2] Log Checker")
 print("- [3] Task List")
 print("- [0] Exit")
 user_select = input("Please select number: ")
 while(user_select!= '0'):
-    print("Coming soon.../n")
+    if user_select == '1':
+        print("Calling show_sysinfo()... ")
+        sysinfo.show_sysinfo()
     print("- [1] System Info")
     print("- [2] Log Checker")
     print("- [3] Task List")
